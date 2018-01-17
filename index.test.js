3676bd321e4522b1c158b68c21f5ef8acce25ea1
Quang Nguyen <qtn2@njit.edu>
2018-01-17 10:59:10 -0500

    updated test

---
+++ index.test.js
@@ -0,0 +1,76 @@
+
+const d3 = require('d3');
+let s = d3.select('body').append('p').text('hello w!!!');
+console.log(s);
+
+  test('it exists', () => {
+    expect(s).not.toBeNull();
+  })
+
+ 
+   test('type of s', () => {
+    expect(typeof s).toBe('object');
+  })
+
+    test('hello world', () => {
+    expect(`${s._groups[0][0]}`).toBe('[object HTMLParagraphElement]')
+  })
+  
+
+  
+//     // describe('the svg' ,function() {
+//     //   it('should be created', function() {
+//     //       expect(getSvg()).not.toBeNull();
+//     //   });
+  
+//     //   it('should have the correct height', function() {
+//     //     expect(getSvg().attr('width')).toBe('500');
+//     //   });
+  
+//     //   it('should have the correct width', function() {
+//     //     expect(getSvg().attr('width')).toBe('500');
+//     //   });
+//     // });
+  
+//     function getSvg() {
+//       return d3.select('p');
+//     }
+
+// let d3 = require('d3');
+// let p = d3.select('body').append('p').text('hello world!!!');
+
+// function hello() {
+//     let that = {};
+
+//     that.render = function() {
+//        let test = d3.select('body').append('p').text('hello world!!!');
+             
+//     };
+
+//     return that;
+// }
+
+// describe('Test D3.js with jasmine ', function() {
+//     var c;
+  
+//     beforeEach(function() {
+//       c = hello();
+//       c.render();
+//     });
+  
+//     afterEach(function() {
+//       d3.selectAll('p').remove();
+//     });
+  
+//     describe('the svg' ,function() {
+//       it('should be created', function() {
+//           expect(getSvg()).not.toBeNull();
+//       });
+  
+//       });
+  
+//     function getSvg() {
+//       return d3.select('p');
+//     }
+  
+//   });
