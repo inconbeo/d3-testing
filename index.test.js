bcfa99ff7dfdf22975d65537a1742b27cdf8a514
Quang Nguyen <qtn2@njit.edu>
2018-01-17 18:38:44 -0500

    added auziliary file and put node_nodules in gitignore

--- index.test.js
+++ index.test.js
@@ -13,64 +13,9 @@ console.log(s);
   })
 
     test('hello world', () => {
-    expect(`${s._groups[0][0]}`).toBe('[object HTMLParagraphElement]')
+      console.log(s);
+    expect(`${s}`).toBe('hellow world')
   })
   
 
   
-//     // describe('the svg' ,function() {
-//     //   it('should be created', function() {
-//     //       expect(getSvg()).not.toBeNull();
-//     //   });
-  
-//     //   it('should have the correct height', function() {
-//     //     expect(getSvg().attr('width')).toBe('500');
-//     //   });
-  
-//     //   it('should have the correct width', function() {
-//     //     expect(getSvg().attr('width')).toBe('500');
-//     //   });
-//     // });
-  
-//     function getSvg() {
-//       return d3.select('p');
-//     }
-
-// let d3 = require('d3');
-// let p = d3.select('body').append('p').text('hello world!!!');
-
-// function hello() {
-//     let that = {};
-
-//     that.render = function() {
-//        let test = d3.select('body').append('p').text('hello world!!!');
-             
-//     };
-
-//     return that;
-// }
-
-// describe('Test D3.js with jasmine ', function() {
-//     var c;
-  
-//     beforeEach(function() {
-//       c = hello();
-//       c.render();
-//     });
-  
-//     afterEach(function() {
-//       d3.selectAll('p').remove();
-//     });
-  
-//     describe('the svg' ,function() {
-//       it('should be created', function() {
-//           expect(getSvg()).not.toBeNull();
-//       });
-  
-//       });
-  
-//     function getSvg() {
-//       return d3.select('p');
-//     }
-  
-//   });
