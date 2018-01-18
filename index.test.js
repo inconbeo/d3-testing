953b68232dd18eb8551ae5aa889654ede5427472
Quang Nguyen <qtn2@njit.edu>
2018-01-18 01:44:21 -0500

    deleted auxiliary file

--- index.test.js
+++ index.test.js
@@ -2,19 +2,19 @@
 const d3 = require('d3');
 let s = d3.select('body').append('p').text('hello world');
 
-   test('it exists', () => {
+test('it exists', () => {
     expect(s).not.toBeNull();
-  })
+})
 
  
-   test('type of s', () => {
+test('type of s', () => {
     expect(typeof s).toBe('object');
-  })
+})
 
-    test('hello world', () => {
-      console.log(d3.select('p').text());
-      expect(d3.select('p').text()).toBe('hello world')
-  })
+test('hello world', () => {
+    console.log(d3.select('p').text());
+    expect(d3.select('p').text()).toBe('hello world')
+})
   
   
 
